dolphin-emu/dolphin

VideoCommon: add graphics mod editor window control for generating a set of materials from some input parameters

Source/Core/VideoCommon/GraphicsModEditor/Controls/MaterialGenerateWindow.h
@@ -1,0 +1,11 @@
+// Copyright 2024 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "VideoCommon/GraphicsModEditor/MaterialGeneration.h"
+
+namespace GraphicsModEditor::Controls
+{
+bool ShowMaterialGenerateWindow(MaterialGenerationContext* context);
+}

TheShrubMaster/dolphin-wiim-audio-fork

Source/Core/VideoCommon/GraphicsModEditor/Controls/MaterialGenerateWindow.h
@@ -1,0 +1,11 @@
+// Copyright 2024 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "VideoCommon/GraphicsModEditor/MaterialGeneration.h"
+
+namespace GraphicsModEditor::Controls
+{
+bool ShowMaterialGenerateWindow(RasterMaterialGenerationContext* context);
+}