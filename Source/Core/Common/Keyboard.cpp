dolphin-emu/dolphin

Common/Keyboard: Add keyboard layout support and partial translation

Source/Core/Common/Keyboard.cpp
@@ -4,6 +4,7 @@
 #include "Common/Keyboard.h"
 
 #include <array>
+#include <map>
 #include <mutex>
 #include <utility>
 
@@ -18,13 +19,126 @@ u32 Common::KeyboardContext::s_sdl_quit_event_type(-1);
 #endif
 
 #include "Core/Config/MainSettings.h"
+#include "Core/Config/SYSCONFSettings.h"
+#include "DiscIO/Enums.h"
 
 namespace
 {
-u8 MapVirtualKeyToHID(u8 virtual_key, int keyboard_layout)
+// Translate HID usage ID based on the host and the game keyboard layout:
+//  - we need to take into account the host layout as we receive raw scan codes
+//  - we need to consider the game layout as it might be different from the host one
+u8 TranslateUsageID(u8 usage_id, int host_layout, int game_layout)
+{
+  if (host_layout == game_layout)
+    return usage_id;
+
+  // Currently, the translation is partial (i.e. alpha only)
+  if (usage_id != Common::HIDUsageID::M_AZERTY &&
+      (usage_id < Common::HIDUsageID::A || usage_id > Common::HIDUsageID::Z))
+  {
+    return usage_id;
+  }
+
+  switch (host_layout | game_layout)
+  {
+  case Common::KeyboardLayout::AZERTY_QWERTZ:
+  {
+    static const std::map<u8, u8> TO_QWERTZ{
+        {Common::HIDUsageID::A_AZERTY, Common::HIDUsageID::A},
+        {Common::HIDUsageID::Z_AZERTY, Common::HIDUsageID::Z_QWERTZ},
+        {Common::HIDUsageID::Y, Common::HIDUsageID::Y_QWERTZ},
+        {Common::HIDUsageID::Q_AZERTY, Common::HIDUsageID::Q},
+        {Common::HIDUsageID::M_AZERTY, Common::HIDUsageID::M},
+        {Common::HIDUsageID::W_AZERTY, Common::HIDUsageID::W},
+        {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY},
+    };
+    static const std::map<u8, u8> TO_AZERTY{
+        {Common::HIDUsageID::Q, Common::HIDUsageID::Q_AZERTY},
+        {Common::HIDUsageID::W, Common::HIDUsageID::W_AZERTY},
+        {Common::HIDUsageID::Z_QWERTZ, Common::HIDUsageID::Z_AZERTY},
+        {Common::HIDUsageID::A, Common::HIDUsageID::A_AZERTY},
+        {Common::HIDUsageID::M_AZERTY, Common::HIDUsageID::M},
+        {Common::HIDUsageID::Y_QWERTZ, Common::HIDUsageID::Y},
+        {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY},
+    };
+    const auto& map = game_layout == Common::KeyboardLayout::QWERTZ ? TO_QWERTZ : TO_AZERTY;
+    if (const auto it{map.find(usage_id)}; it != map.end())
+      return it->second;
+    break;
+  }
+  case Common::KeyboardLayout::QWERTY_AZERTY:
+  {
+    static constexpr std::array<std::pair<u8, u8>, 3> BI_MAP{
+        {{Common::HIDUsageID::Q, Common::HIDUsageID::A},
+         {Common::HIDUsageID::W, Common::HIDUsageID::Z},
+         {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY}}};
+    for (const auto& [a, b] : BI_MAP)
+    {
+      if (usage_id == a)
+        return b;
+      else if (usage_id == b)
+        return a;
+    }
+    break;
+  }
+  case Common::KeyboardLayout::QWERTY_QWERTZ:
+  {
+    if (usage_id == Common::HIDUsageID::Y)
+      return Common::HIDUsageID::Z;
+    else if (usage_id == Common::HIDUsageID::Z)
+      return Common::HIDUsageID::Y;
+    break;
+  }
+  default:
+    // Shouldn't happen
+    break;
+  }
+  return usage_id;
+}
+
+int GetHostLayout()
+{
+  const int layout = Config::Get(Config::MAIN_WII_KEYBOARD_HOST_LAYOUT);
+  if (layout != Common::KeyboardLayout::AUTO)
+    return layout;
+
+#ifdef HAVE_SDL2
+  if (const SDL_Keycode key_code = SDL_GetKeyFromScancode(SDL_SCANCODE_Y); key_code == SDLK_z)
+  {
+    return Common::KeyboardLayout::QWERTZ;
+  }
+  if (const SDL_Keycode key_code = SDL_GetKeyFromScancode(SDL_SCANCODE_Q); key_code == SDLK_a)
+  {
+    return Common::KeyboardLayout::AZERTY;
+  }
+#endif
+
+  return Common::KeyboardLayout::QWERTY;
+}
+
+int GetGameLayout()
+{
+  const int layout = Config::Get(Config::MAIN_WII_KEYBOARD_GAME_LAYOUT);
+  if (layout != Common::KeyboardLayout::AUTO)
+    return layout;
+
+  const DiscIO::Language language =
+      static_cast<DiscIO::Language>(Config::Get(Config::SYSCONF_LANGUAGE));
+  switch (language)
+  {
+  case DiscIO::Language::French:
+    return Common::KeyboardLayout::AZERTY;
+  case DiscIO::Language::German:
+    return Common::KeyboardLayout::QWERTZ;
+  default:
+    return Common::KeyboardLayout::QWERTY;
+  }
+}
+
+u8 MapVirtualKeyToHID(u8 virtual_key, int host_layout, int game_layout)
 {
   // SDL2 keyboard state uses scan codes already based on HID usage id
-  return virtual_key;
+  return TranslateUsageID(virtual_key, host_layout, game_layout);
 }
 
 std::weak_ptr<Common::KeyboardContext> s_keyboard_context;
@@ -57,6 +171,7 @@ void KeyboardContext::Init()
   SDL_PushEvent(&event);
   m_keyboard_state = SDL_GetKeyboardState(nullptr);
 #endif
+  UpdateLayout();
   m_is_ready = true;
 }
 
@@ -99,6 +214,15 @@ void KeyboardContext::NotifyQuit()
     self->Quit();
 }
 
+void KeyboardContext::UpdateLayout()
+{
+  if (auto self = s_keyboard_context.lock())
+  {
+    self->m_host_layout = GetHostLayout();
+    self->m_game_layout = GetGameLayout();
+  }
+}
+
 const void* KeyboardContext::GetWindowHandle()
 {
   return s_handler_state.GetHandle();
@@ -116,10 +240,10 @@ std::shared_ptr<KeyboardContext> KeyboardContext::GetInstance()
   return ptr;
 }
 
-HIDPressedState KeyboardContext::GetPressedState(int keyboard_layout) const
+HIDPressedState KeyboardContext::GetPressedState() const
 {
   return m_is_ready ? HIDPressedState{.modifiers = PollHIDModifiers(),
-                                      .pressed_keys = PollHIDPressedKeys(keyboard_layout)} :
+                                      .pressed_keys = PollHIDPressedKeys()} :
                       HIDPressedState{};
 }
 
@@ -172,7 +296,7 @@ u8 KeyboardContext::PollHIDModifiers() const
   return modifiers;
 }
 
-HIDPressedKeys KeyboardContext::PollHIDPressedKeys(int keyboard_layout) const
+HIDPressedKeys KeyboardContext::PollHIDPressedKeys() const
 {
   HIDPressedKeys pressed_keys{};
   auto it = pressed_keys.begin();
@@ -190,7 +314,7 @@ HIDPressedKeys KeyboardContext::PollHIDPressedKeys(int keyboard_layout) const
     if (!IsVirtualKeyPressed(static_cast<int>(virtual_key)))
       continue;
 
-    *it = MapVirtualKeyToHID(static_cast<u8>(virtual_key), keyboard_layout);
+    *it = MapVirtualKeyToHID(static_cast<u8>(virtual_key), m_host_layout, m_game_layout);
     if (++it == pressed_keys.end())
       break;
   }

Source/Core/InputCommon/ControllerInterface/SDL/SDL.cpp
@@ -47,6 +47,9 @@ std::optional<const char*> UpdateKeyboardHandle(UniqueSDLWindow* unique_window)
     SDL_SetWindowFullscreen(keyboard_window, 0);
     SDL_SetWindowBordered(keyboard_window, SDL_TRUE);
   }
+
+  Common::KeyboardContext::UpdateLayout();
+
   return error;
 }
 }  // namespace
@@ -345,6 +348,10 @@ bool InputBackend::HandleEventAndContinue(const SDL_Event& e)
   {
     m_keyboard_window.reset();
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
+  }
+  else if (e.type == SDL_KEYMAPCHANGED)
+  {
+    Common::KeyboardContext::UpdateLayout();
   }
 
   return true;

TheShrubMaster/dolphin-wiim-audio-fork

Source/Core/Common/Keyboard.cpp
@@ -4,6 +4,7 @@
 #include "Common/Keyboard.h"
 
 #include <array>
+#include <map>
 #include <mutex>
 #include <utility>
 
@@ -18,13 +19,128 @@ u32 Common::KeyboardContext::s_sdl_quit_event_type(-1);
 #endif
 
 #include "Core/Config/MainSettings.h"
+#include "Core/Config/SYSCONFSettings.h"
+#include "DiscIO/Enums.h"
 
 namespace
 {
-u8 MapVirtualKeyToHID(u8 virtual_key, int keyboard_layout)
+// Translate HID usage ID based on the host and the game keyboard layout:
+//  - we need to take into account the host layout as we receive raw scan codes
+//  - we need to consider the game layout as it might be different from the host one
+u8 TranslateUsageID(u8 usage_id, int host_layout, int game_layout)
+{
+  if (host_layout == game_layout)
+    return usage_id;
+
+  // Currently, the translation is partial (i.e. alpha only)
+  if (usage_id != Common::HIDUsageID::M_AZERTY &&
+      (usage_id < Common::HIDUsageID::A || usage_id > Common::HIDUsageID::Z))
+  {
+    return usage_id;
+  }
+
+  switch (host_layout | game_layout)
+  {
+  case Common::KeyboardLayout::AZERTY_QWERTZ:
+  {
+    static const std::map<u8, u8> TO_QWERTZ{
+        {Common::HIDUsageID::A_AZERTY, Common::HIDUsageID::A},
+        {Common::HIDUsageID::Z_AZERTY, Common::HIDUsageID::Z_QWERTZ},
+        {Common::HIDUsageID::Y, Common::HIDUsageID::Y_QWERTZ},
+        {Common::HIDUsageID::Q_AZERTY, Common::HIDUsageID::Q},
+        {Common::HIDUsageID::M_AZERTY, Common::HIDUsageID::M},
+        {Common::HIDUsageID::W_AZERTY, Common::HIDUsageID::W},
+        {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY},
+    };
+    static const std::map<u8, u8> TO_AZERTY{
+        {Common::HIDUsageID::Q, Common::HIDUsageID::Q_AZERTY},
+        {Common::HIDUsageID::W, Common::HIDUsageID::W_AZERTY},
+        {Common::HIDUsageID::Z_QWERTZ, Common::HIDUsageID::Z_AZERTY},
+        {Common::HIDUsageID::A, Common::HIDUsageID::A_AZERTY},
+        {Common::HIDUsageID::M_AZERTY, Common::HIDUsageID::M},
+        {Common::HIDUsageID::Y_QWERTZ, Common::HIDUsageID::Y},
+        {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY},
+    };
+    const auto& map = game_layout == Common::KeyboardLayout::QWERTZ ? TO_QWERTZ : TO_AZERTY;
+    if (const auto it{map.find(usage_id)}; it != map.end())
+      return it->second;
+    break;
+  }
+  case Common::KeyboardLayout::QWERTY_AZERTY:
+  {
+    static constexpr std::array<std::pair<u8, u8>, 3> BI_MAP{
+        {{Common::HIDUsageID::Q, Common::HIDUsageID::A},
+         {Common::HIDUsageID::W, Common::HIDUsageID::Z},
+         {Common::HIDUsageID::M, Common::HIDUsageID::M_AZERTY}}};
+    for (const auto& [a, b] : BI_MAP)
+    {
+      if (usage_id == a)
+        return b;
+      else if (usage_id == b)
+        return a;
+    }
+    break;
+  }
+  case Common::KeyboardLayout::QWERTY_QWERTZ:
+  {
+    if (usage_id == Common::HIDUsageID::Y)
+      return Common::HIDUsageID::Z;
+    else if (usage_id == Common::HIDUsageID::Z)
+      return Common::HIDUsageID::Y;
+    break;
+  }
+  default:
+    // Shouldn't happen
+    break;
+  }
+  return usage_id;
+}
+
+int GetHostLayout()
+{
+  const int layout = Config::Get(Config::MAIN_WII_KEYBOARD_HOST_LAYOUT);
+  if (layout != Common::KeyboardLayout::AUTO)
+    return layout;
+
+#ifdef HAVE_SDL3
+  if (const SDL_Keycode key_code = SDL_GetKeyFromScancode(SDL_SCANCODE_Y, SDL_KMOD_NONE, false);
+      key_code == SDLK_Z)
+  {
+    return Common::KeyboardLayout::QWERTZ;
+  }
+  if (const SDL_Keycode key_code = SDL_GetKeyFromScancode(SDL_SCANCODE_Q, SDL_KMOD_NONE, false);
+      key_code == SDLK_A)
+  {
+    return Common::KeyboardLayout::AZERTY;
+  }
+#endif
+
+  return Common::KeyboardLayout::QWERTY;
+}
+
+int GetGameLayout()
+{
+  const int layout = Config::Get(Config::MAIN_WII_KEYBOARD_GAME_LAYOUT);
+  if (layout != Common::KeyboardLayout::AUTO)
+    return layout;
+
+  const DiscIO::Language language =
+      static_cast<DiscIO::Language>(Config::Get(Config::SYSCONF_LANGUAGE));
+  switch (language)
+  {
+  case DiscIO::Language::French:
+    return Common::KeyboardLayout::AZERTY;
+  case DiscIO::Language::German:
+    return Common::KeyboardLayout::QWERTZ;
+  default:
+    return Common::KeyboardLayout::QWERTY;
+  }
+}
+
+u8 MapVirtualKeyToHID(u8 virtual_key, int host_layout, int game_layout)
 {
   // SDL3 keyboard state uses scan codes already based on HID usage id
-  return virtual_key;
+  return TranslateUsageID(virtual_key, host_layout, game_layout);
 }
 
 std::weak_ptr<Common::KeyboardContext> s_keyboard_context;
@@ -57,6 +173,7 @@ void KeyboardContext::Init()
   SDL_PushEvent(&event);
   m_keyboard_state = SDL_GetKeyboardState(nullptr);
 #endif
+  UpdateLayout();
   m_is_ready = true;
 }
 
@@ -99,6 +216,15 @@ void KeyboardContext::NotifyQuit()
     self->Quit();
 }
 
+void KeyboardContext::UpdateLayout()
+{
+  if (auto self = s_keyboard_context.lock())
+  {
+    self->m_host_layout = GetHostLayout();
+    self->m_game_layout = GetGameLayout();
+  }
+}
+
 void* KeyboardContext::GetWindowHandle()
 {
   return s_handler_state.GetHandle();
@@ -116,10 +242,10 @@ std::shared_ptr<KeyboardContext> KeyboardContext::GetInstance()
   return ptr;
 }
 
-HIDPressedState KeyboardContext::GetPressedState(int keyboard_layout) const
+HIDPressedState KeyboardContext::GetPressedState() const
 {
   return m_is_ready ? HIDPressedState{.modifiers = PollHIDModifiers(),
-                                      .pressed_keys = PollHIDPressedKeys(keyboard_layout)} :
+                                      .pressed_keys = PollHIDPressedKeys()} :
                       HIDPressedState{};
 }
 
@@ -172,7 +298,7 @@ u8 KeyboardContext::PollHIDModifiers() const
   return modifiers;
 }
 
-HIDPressedKeys KeyboardContext::PollHIDPressedKeys(int keyboard_layout) const
+HIDPressedKeys KeyboardContext::PollHIDPressedKeys() const
 {
   HIDPressedKeys pressed_keys{};
   auto it = pressed_keys.begin();
@@ -190,7 +316,7 @@ HIDPressedKeys KeyboardContext::PollHIDPressedKeys(int keyboard_layout) const
     if (!IsVirtualKeyPressed(static_cast<int>(virtual_key)))
       continue;
 
-    *it = MapVirtualKeyToHID(static_cast<u8>(virtual_key), keyboard_layout);
+    *it = MapVirtualKeyToHID(static_cast<u8>(virtual_key), m_host_layout, m_game_layout);
     if (++it == pressed_keys.end())
       break;
   }

Source/Core/InputCommon/ControllerInterface/SDL/SDL.cpp
@@ -75,6 +75,9 @@ std::optional<const char*> UpdateKeyboardHandle(UniqueSDLWindow* unique_window)
     SDL_SetWindowFullscreen(keyboard_window, 0);
     SDL_SetWindowBordered(keyboard_window, true);
   }
+
+  Common::KeyboardContext::UpdateLayout();
+
   return error;
 }
 }  // namespace
@@ -381,6 +384,10 @@ bool InputBackend::HandleEventAndContinue(const SDL_Event& e)
   {
     m_keyboard_window.reset();
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
+  }
+  else if (e.type == SDL_EVENT_KEYMAP_CHANGED)
+  {
+    Common::KeyboardContext::UpdateLayout();
   }
 
   return true;